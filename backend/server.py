7df06ff0e4e8503309d823ee5fe662686f7131be
aca166 <aca166@sfu.ca>
2019-09-06 15:21:10 -0700

    connected frontend and backend; fixed CORS error by using flask-cors

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 import connexion
+from flask_cors import CORS
 
 app = connexion.App(__name__, specification_dir='./')
+CORS(app.app)
 
 # Create the application instance
 app.add_api('swagger.yml')
--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,6 +1,29 @@
+
+
 app = new Vue({
     el: ".main",
     data: {
-        hello: 'hello world'
+        hello: "hello world",
+        info: null,
+        input_: "",
+    },
+    methods: {
+        parseInput(inp) {
+            re = new RegExp("\ ", "g")
+            inp.replace(re, "+")
+            return inp
+        },
+        articleSearch(event, inp) {
+            console.log(event);
+            cleanedInp = this.parseInput(inp)
+            axios
+                .get(`http://localhost:5000/api/search?title=${cleanedInp}`)
+                .then(response => (this.info = response))
+
+        },
+
+    },
+    computed: { 
+
     }
 })
