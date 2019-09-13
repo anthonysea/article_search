94a73008dfc88e8d9443f6821e0621d79577bb4e
anthonysea <anthonyqchao@gmail.com>
2019-09-12 22:09:48 -0700

    Refined client-side validation and error handling

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -7,6 +7,7 @@ app = new Vue({
         articles: null,
         input_: "",
         numberOfResults: null,
+        errors: [],
     },
     methods: {
         parseQuery(inp) {
@@ -30,16 +31,56 @@ app = new Vue({
             return inp
         },
         parseNumberOfResults() {
+            numResInt = parseInt(this.numberOfResults)
 
+            // Check if there is an input at all and if the input is a valid integer
+            if (!Number.isInteger(numResInt) && this.numberOfResults && this.numberOfResults.includes(".")) {
+                this.errors.push("Please enter an integer between 1 and 50 (inclusive) or leave blank for 10")
+                console.log("error1")
+                return false
+            } else {
+                // If there is no input
+                if (!this.numberOfResults) {
+                    this.numberOfResults = 10
+                    this.errors = []
+                    return true
+                // Input is a valid number between 1 and 50
+                } else if (Number.isInteger(numResInt) && (1 <= numResInt) && (numResInt <= 50)) {
+                    this.errors = []
+                    this.numberOfResults = numResInt
+                    return true
+                } else {
+                    this.errors.push("Please enter an integer between 1 and 50 (inclusive) or leave blank for 10")
+                    // this.errors[0] = "Please enter an integer between 1 and 50 (inclusive) or leave blank for 10"
+                    console.log("error2")
+                    return false
+                }
+            }
+        
+            // if (Number.isInteger(this.numberOfResults) && (1 <= this.numberOfResults) && (this.numberOfResults <= 50)) {
+            //     this.errors = []
+            //     console.log('working')
+            //     return true
+            // } else if (!this.numberOfResults) {
+            //     this.numberOfResults = 10
+            //     this.errors = []
+            //     console.log('branch1')
+            //     return true
+            // } else {
+            //     // this.errors[0] = "Please enter an integer between 1 and 50 (inclusive) or leave blank for 10"
+            //     this.errors.push("Please enter an integer between 1 and 50 (inclusive) or leave blank for 10")
+            //     console.log('error')
+            //     return false
+            // }
         },
         articleSearch(event, inp) {
             console.log(event);
-            cleanedInp = this.parseQuery(inp)
-            if (!this.numberOfResults) {
-                this.numberOfResults = 10;
+            cleanedQuery = this.parseQuery(inp)
+            if (!this.parseNumberOfResults()) {
+                return
             }
             axios
-                .get(`http://localhost:5000/api/search?title=${cleanedInp}&numberOfResults=${this.numberOfResults}`)
+                .get(`http://localhost:5000/api/search?title=${cleanedQuery}&numberOfResults=${this.numberOfResults}`)
                 .then(response => (this.articles = response.data))
         },
 
