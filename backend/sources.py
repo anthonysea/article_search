e865a6709dc3e1222445c93ecf10de296b3c63fb
anthonysea <anthonyqchao@gmail.com>
2019-12-31 12:29:03 -0800

    refactored backend to accomodate changes to html of results; added user-agent header to request; added date entry into results

--- backend/sources.py
+++ backend/sources.py
@@ -6,15 +6,19 @@ def extractSearchResults(title, numberOfResults):
     results = []
     url = f"https://www.google.com/search?query={title}&num={numberOfResults}&tbm=nws"
     print(url)
-    r = requests.get(url)
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+    headers = {'User-Agent': user_agent}
+    r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.content, "html.parser")
     # Results are in a div.g
-    div = soup.findAll('div', {'class': 'g'})
+    div = soup.find_all('div', {'class': 'g'})
+    print("soup", soup)
+    print("div", div)
 
     for res in div:
         result = {}
         # Extract the title of the article
-        result['title'] = res.select('a')[0].text
+        result['title'] = res.select('a')[1].text
 
         # Extract the url of the article, need to strip extra query parameters
         # that Google adds to the href attribute
@@ -24,7 +28,9 @@ def extractSearchResults(title, numberOfResults):
         result['url'] = result_url_str
 
         # Get the news source
-        result['source'] = res.find('span', class_='f').text.split(" - ")[0]
+        result['source'] = res.find('div', class_='slp').text.split("-")[0]
+        # Get the time of publication
+        result['date'] = res.find('div', class_='slp').text.split("-")[1]
 
         results.append(result)
     
